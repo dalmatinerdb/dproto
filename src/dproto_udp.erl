86d88bb2394c60b7db37ea28ddf7ae29b733c456
Heinz N. Gies <heinz@licenser.net>
2014-06-29 03:19:28 +0200

    Fixed udp put to not include prefix.

--- src/dproto_udp.erl
+++ src/dproto_udp.erl
@@ -10,7 +10,7 @@ encode_points(Bucket, Metric, Time, Points) when is_list(Points) ->
     encode_points(Bucket, Metric, Time, << <<1, V:64/integer>> || V <-  Points >>);
 
 encode_points(Bucket, Metric, Time, Points) when is_binary(Points) ->
-    <<?PUT, Time:?TIME_SIZE/integer,
+    <<Time:?TIME_SIZE/integer,
       (byte_size(Bucket)):?BUCKET_SS/integer, Bucket/binary,
       (byte_size(Metric)):?METRIC_SS/integer, Metric/binary,
       (byte_size(Metric)):?DATA_SS/integer, Points/binary>>.
