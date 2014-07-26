93af6ce78e71273f908d551abd9ae4561552613b
Heinz N. Gies <heinz@licenser.net>
2014-07-26 16:53:09 +0200

    Updated udp proto for allowing additional packages.

--- src/dproto_udp.erl
+++ src/dproto_udp.erl
@@ -1,16 +1,20 @@
 -module(dproto_udp).
 -include("dproto.hrl").
 
--export([encode_points/4]).
+-export([encode_header/1, encode_points/3]).
 
-encode_points(Bucket, Metric, Time, Point) when is_integer(Point) ->
-    encode_points(Bucket, Metric, Time, <<1, Point:64/integer>>);
+encode_header(Bucket) ->
+    <<?PUT, (byte_size(Bucket)):?BUCKET_SS/integer,
+      Bucket/binary>>.
 
-encode_points(Bucket, Metric, Time, Points) when is_list(Points) ->
-    encode_points(Bucket, Metric, Time, << <<1, V:64/integer>> || V <-  Points >>);
+encode_points(Metric, Time, Point) when is_integer(Point) ->
+    encode_points(Metric, Time, <<1, Point:64/integer>>);
 
-encode_points(Bucket, Metric, Time, Points) when is_binary(Points) ->
+encode_points(Metric, Time, Points) when is_list(Points) ->
+    encode_points(Metric, Time, << <<1, V:64/integer>> || V <-  Points >>);
+
+encode_points(Metric, Time, Points) when is_binary(Points) ->
     <<Time:?TIME_SIZE/integer,
-      (byte_size(Bucket)):?BUCKET_SS/integer, Bucket/binary,
       (byte_size(Metric)):?METRIC_SS/integer, Metric/binary,
       (byte_size(Points)):?DATA_SS/integer, Points/binary>>.
+
