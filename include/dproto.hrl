77e9cc99a2952281bb8f8a2ea29bc37144e056ad
Heinz N. Gies <heinz@licenser.net>
2014-12-08 08:03:25 +0100

    Added stream defines.

--- include/dproto.hrl
+++ include/dproto.hrl
@@ -1,8 +1,12 @@
--define(KEEPALIVE, 0).
 -define(PUT, 0).
 -define(LIST, 1).
 -define(GET, 2).
 -define(BUCKETS, 3).
+-define(STREAM, 4).
+
+-define(SENTRY, 1).
+-define(SWRITE, 2).
+
 -define(BUCKET_SS, 8).
 -define(METRIC_ELEM_SS, 8).
 -define(METRIC_SS, 16).
