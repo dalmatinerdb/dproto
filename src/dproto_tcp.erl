b1776af311f0e9da8d972c245080a93fff5c9103
Heinz N. Gies <heinz@licenser.net>
2014-12-08 09:12:42 +0100

    Added encoders for stream API.

--- src/dproto_tcp.erl
+++ src/dproto_tcp.erl
@@ -5,7 +5,10 @@
 -export([
          encode_metrics/1, decode_metrics/1,
          encode_get/4, decode_get/1,
-         decode_list/1, encode_list/1
+         decode_list/1, encode_list/1,
+         encode_start_stream/2,
+         encode_stream_flush/0,
+         encode_stream_payload/3
         ]).
 
 encode_metrics(Ms) ->
@@ -31,9 +34,25 @@ encode_get(B, M, T, C) ->
       (byte_size(M)):?METRIC_SS/integer, M/binary,
       T:?TIME_SIZE/integer, C:?COUNT_SIZE/integer>>.
 
-
 decode_list(<<_LB:?BUCKET_SS/integer, B:_LB/binary>>) ->
     B.
 
 encode_list(Bucket) ->
     <<(byte_size(Bucket)):?BUCKET_SS/integer, Bucket/binary>>.
+
+encode_start_stream(Delay, Bucket) when Delay > 0, Delay < 256,
+                                        is_binary(Bucket) ->
+    <<?STREAM, Delay:8, Bucket/binary>>.
+
+encode_stream_flush() ->
+    <<?SWRITE>>.
+
+encode_stream_payload(Metric, Time, Points) when is_list(Points) ->
+    encode_stream_payload(Metric, Time, encode_metrics(Points));
+
+encode_stream_payload(Metric, Time, Points) when is_binary(Points),
+                                                 is_binary(Metric) ->
+    <<?SENTRY,
+      Time:?TIME_SIZE/integer,
+      (byte_size(Metric)):?METRIC_SS/integer, Metric/binary,
+      (byte_size(Points)):?DATA_SS/integer, Points/binary>>.
